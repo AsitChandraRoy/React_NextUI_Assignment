c67b42940f33f2982cbabc80c9675108ad9a67d3
AsitChandraRoy <asitchandraroy@gmail.com>
2024-11-27 23:48:01 +0600

    responsiveness of hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,15 +5,15 @@ function HeroSection() {
   return (
     <>
         <div className='max-w-full'>
-            <img className='absolute w-[100%] h-[450px] ' src="Img.png" alt="" />
+            <img className='absolute w-[100%] sm:h-[450px] ' src="Img.png" alt="" />
         </div>
-        <section className='mx-auto p-14 font-montserrat'>
+        <section className='mx-auto p-14 font-montserrat sm:text-start text-center'>
             <div className='relative'>
             <h4 className='text-[#0ff1f6] font-light'>AI SDRs (aiDRs)</h4>
-            <h1 className='text-[64px] font-bold -mt-3'>More <i>leads,</i></h1>
-            <h1 className='text-[64px] font-bold -mt-5'>less <i>people.</i></h1>
-            <p className='text-[20px] font-light w-[490px] font-work'>Train an aiDR on your ICP and messaging matrix. Activate it on a patch. It will send personalized sequences to every target contact.</p>
-            <Button as={Link} className="bg-[#0ff1f6] mt-5 font-work" radius="full">
+            <h1 className='sm:text-[64px] font-bold py-0 sm:py-5 text-3xl'>More <i>leads,</i></h1>
+            <h1 className='sm:text-[64px] font-bold py-0 sm:py-5 text-3xl'>less <i>people.</i></h1>
+            <p className='text-sm sm:text-[20px] font-light sm:w-[490px] font-work mt-3'>Train an aiDR on your ICP and messaging matrix. Activate it on a patch. It will send personalized sequences to every target contact.</p>
+            <Button as={Link} className="bg-[#0ff1f6] mt-5 font-work text-xs sm:text-small w-64" radius="full">
             Sign Up for the Beta
             <img src="arrow.png" alt="" />
             </Button>
