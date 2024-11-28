4e00187cca098051aca91174fa15e6a051c75210
AsitChandraRoy <asitchandraroy@gmail.com>
2024-11-29 01:06:19 +0600

    problem solved from comment section

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import HeroSection from './Components/Hero'
 import PartnerSection from './Components/Partners'
 import ControlScalableFlexible from './Components/ControlScalableFlexible'
 import TrainSection from './Components/Train'
-import CommentSection from './Components/comment'
 import Allocate from './Components/Allocate'
 import GetStarted from './Components/GetStarted'
 import Footer from './Components/Footer'
+import CommentSection from './Components/comment'
 
 function App() {
 
--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,33 +1,44 @@
-import React from 'react'
+import React from "react";
 
 function CommentSection() {
   return (
     <>
-        <section className='py-20 font-work'>
-        <div className=' flex justify-center items-center'>
-          <img className='w-12' src="comment.png" alt="" />
+      <section className="py-20 font-work">
+        <div className=" flex justify-center items-center">
+          <img className="w-12" src="comment.png" alt="" />
         </div>
-        <div className='grid grid-cols-12 text-center'>
-          <div className='col-span-1 sm:col-span-2 flex justify-center items-center'>
-            <img className='sm:w-7 w-0' src="arrow2.png" alt="" />
+        <div className="grid grid-cols-12 text-center">
+          <div className="col-span-1 sm:col-span-2 flex justify-center items-center">
+            <img className="sm:w-7 w-0" src="arrow2.png" alt="" />
           </div>
-          <div className='col-span-10 sm:col-span-8 py-7'>
-            <p className='text-[24px] sm:text[32px] font-light'>Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
-            <p className='pt-5 text-[18px] sm:text-2xl font-semibold text-[#0ff1f6]'>John Doe</p>
-            <p className=' text-[18px] sm:text-2xl font-semibold opacity-50'>Chief Strategy Officer @ Company</p>
+          <div className="col-span-10 sm:col-span-8 py-7">
+            <p className="text-[24px] sm:text[32px] font-light">
+              Qorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              vulputate libero et velit interdum, ac aliquet odio mattis.
+            </p>
+            <p className="pt-5 text-[18px] sm:text-2xl font-semibold text-[#0ff1f6]">
+              John Doe
+            </p>
+            <p className=" text-[18px] sm:text-2xl font-semibold opacity-50">
+              Chief Strategy Officer @ Company
+            </p>
           </div>
-          <div className='col-span-1 sm:col-span-2 flex justify-center items-center'>
-            <img className='col-span-1 sm:col-span-2 sm:w-7 w-0' src="arrow3.png" alt="" />
+          <div className="col-span-1 sm:col-span-2 flex justify-center items-center">
+            <img
+              className="col-span-1 sm:col-span-2 sm:w-7 w-0"
+              src="arrow3.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex justify-center items-center gap-4'>
-            <div className='h-2 w-2 rounded-full bg-[#0ff1f6]'></div>
-            <div className='h-2 w-2 rounded-full bg-[#394648]'></div>
-            <div className='h-2 w-2 rounded-full bg-[#394648]'></div>
-          </div>
-        </section>
+        <div className="flex justify-center items-center gap-4">
+          <div className="h-2 w-2 rounded-full bg-[#0ff1f6]"></div>
+          <div className="h-2 w-2 rounded-full bg-[#394648]"></div>
+          <div className="h-2 w-2 rounded-full bg-[#394648]"></div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default CommentSection
+export default CommentSection;
